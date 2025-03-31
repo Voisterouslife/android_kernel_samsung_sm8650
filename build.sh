ebd9a7554eb750feebe7fe69404ca45f7c137f9f
YuzakiKokuban <heibanbaize@gmail.com>
2025-03-31 11:02:33 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ TARGET_DEFCONFIG=${1:-pineapple_gki_defconfig}
 
 cd "$(dirname "$0")"
 
-LOCALVERSION=-android14-Kokuban-Elysia-AYB6
+LOCALVERSION=-android14-Kokuban-Elysia-AYB6-SukiSU
 
 if [ "$LTO" == "thin" ]; then
   LOCALVERSION+="-thin"
