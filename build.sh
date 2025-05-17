675522b8937aa0f884325bb18bcb47299ae0f09f
YuzakiKokuban <heibanbaize@gmail.com>
2025-05-17 09:06:45 +0800

    升级到BYDA

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ TARGET_DEFCONFIG=${1:-pineapple_gki_defconfig}
 
 cd "$(dirname "$0")"
 
-LOCALVERSION=-android14-Kokuban-Elysia-BYD9-LKM
+LOCALVERSION=-android14-Kokuban-Elysia-BYDA-LKM
 
 if [ "$LTO" == "thin" ]; then
   LOCALVERSION+="-thin"
