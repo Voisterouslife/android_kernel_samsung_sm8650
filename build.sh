b854329783d2ab98f2050a2b4feba4e917866853
YuzakiKokuban <heibanbaize@gmail.com>
2025-06-14 23:18:11 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -54,6 +54,7 @@ if [ ! -d AnyKernel3 ]; then
   git clone --depth=1 https://github.com/YuzakiKokuban/AnyKernel3.git -b pineapple
 fi
 cp arch/arm64/boot/Image AnyKernel3/Image
+name=S24_kernel_`cat include/config/kernel.release`_`date '+%Y_%m_%d'`
 cd AnyKernel3
 chmod +x ./patch_linux
 ./patch_linux
@@ -61,7 +62,6 @@ mv oImage zImage
 rm -f oImage
 rm -f Image
 rm -f patch_linux
-name=S24_kernel_`cat include/config/kernel.release`_`date '+%Y_%m_%d'`
 zip -r ${name}.zip * -x *.zip
 cd ..
 cp AnyKernel3/zImage AnyKernel3/tools/kernel
