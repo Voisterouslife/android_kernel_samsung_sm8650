b29ebec0ceab91318689f63f19334fc008b7c9bf
YuzakiKokuban <heibanbaize@gmail.com>
2025-07-03 21:50:21 +0800

    升级SukiSU-Ultra

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ MAIN_DEFCONFIG=pineapple_gki_defconfig
 
 # 2. 内核版本标识
 # 构建系统会自动附加 git commit hash
-LOCALVERSION_BASE=-android14-Kokuban-Elysia-AYD9-SukiSUU
+LOCALVERSION_BASE=-android14-Kokuban-Elysia-AYB6-SukiSUU
 
 # 3. LTO (Link Time Optimization)
 # 设置为 "full", "thin" 或 "" (留空以禁用)
