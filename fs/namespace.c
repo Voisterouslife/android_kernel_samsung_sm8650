dd3432f317fd1ebdb076ae7f022598faee132be6
Voisterouslife <hjk1478523@gmail.com>
2026-08-12 16:52:31 +0800

    update susfs

--- fs/namespace.c
+++ fs/namespace.c
@@ -35,6 +35,7 @@
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 #include <linux/susfs_def.h>
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+ 
 #include <linux/fslog.h>
 #ifdef CONFIG_KDP_NS
 #include <linux/kdp.h>
